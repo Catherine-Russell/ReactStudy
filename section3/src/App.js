0a9f3216f859dc1a8183a61d71241788b6ccb9ce
Catherine-Russell <catherinerussell123@hotmail.co.uk>
2024-04-27 23:59:59 +0100

    complete section 3

--- section3/src/App.js
+++ section3/src/App.js
@@ -2,25 +2,41 @@ import React from "react";
 import { Header } from "./components/Header";
 import { MainGoal } from "./components/MainGoal";
 import { BestPokemon } from "./components/BestPokemon";
-import pokemon from "./pokeData";
+import pokeData from "./pokeData";
 import { InformationSection } from "./components/InformationSection/InformationSection";
 
 function App() {
+  const DUMMY_TODOS = ["Learn React", "Practice React", "Profit!"];
+  function Todo({ text }) {
+    return <li>{text}</li>;
+  }
   return (
     <div>
-      <Header />
+      <>
+        {DUMMY_TODOS[0]}
+        {DUMMY_TODOS.map((item) => (
+          <>
+            <p>hello {item}</p>
+            <Todo text={item} />
+          </>
+        ))}
+        {/* {DUMMY_TODOS.map((item) => {
+            <p>{item}</p>;
+          })} */}
+      </>
+      ;
+      {/* <Header />
       <main id="best-practice">
         <h2>Time to get started!</h2>
         <MainGoal />
         <InformationSection />
         <section id="best-pokemon">
           <h2>Best Pokemon</h2>
-          <BestPokemon {...pokemon.pikachu} />
-          <BestPokemon {...pokemon.arcanine} />
-          <BestPokemon {...pokemon.lapras} />
-          <BestPokemon {...pokemon.eevee} />
+          {pokeData.map((pokemon) => (
+            <BestPokemon key={pokemon.name} {...pokemon} />
+          ))}
         </section>
-      </main>
+      </main> */}
     </div>
   );
 }
